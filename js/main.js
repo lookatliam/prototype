874e1ae2266f3032173704a6de36901ffee50f16
liam <lookatliam@gmail.com>
2016-07-11 16:04:58 +0300

    SAVE project

--- js/main.js
+++ js/main.js
@@ -1,58 +1,74 @@
-function scrollResize() {
-	var windowHeight = $(window).height();
-	var maxHeight = $(window).height() - $(".top").height();
-	var datailsHeight = windowHeight - 300 + $(this).scrollTop();
-	if (datailsHeight >= maxHeight) {
-		datailsHeight = maxHeight;
-	}
-	$(".details").height(datailsHeight);
-	$(".chat").height(datailsHeight);
-
-	//margin left function
-	var leftMargin = ($(window).width() - $(".container").width())/2 + $(".window").width();
-	var leftCenterMargin = ($(window).width() - $(".container").width())/2 + $(".window").width() + $(".details").width();
-	var windowWidth = $(".window").width();
-	$(".details").css({"left": leftMargin});
-	$(".chat").css({"left": leftCenterMargin});
-
-	var containerWidth = $(".container").width();
-	if(containerWidth >= 900 && containerWidth <= 1200) {
-		$(".chat").hide();
-		var coef = containerWidth/2;
-
-		$(".window").width(coef*1.25);
-		$(".details").width(coef*0.75);
-	} else if (containerWidth > 1200) {
-		$(".chat").show();
-		var coef = containerWidth/4;
-		$(".window").width(coef*1.54);
-		$(".details").width(coef*1.23);
-		$(".chat").width(coef*1.23);
-	} else if(containerWidth < 900) {
-		$(".window").width(containerWidth);
-		console.log(containerWidth);
-		$(".details").hide();
-		$(".chat").hide();
-	}
-}
-$(window).scroll(function() {
-	scrollResize();
-});
-
-$(window).resize(function() {
-	scrollResize();
-});
-
-$(document).ready(function() {
-	scrollResize();
-});
-
-
-
-
-
-//test function
+//test function html append
 for(var n = 0; n < 100; n++) {
 	$(".window").append(n+"<br>")
 }
 
+function main() {
+	//var params
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+	var windowClassWidth = $(".window").width();
+	var maxHeight = windowHeight - $(".top").height();
+	var blocksHeight = windowHeight + $(this).scrollTop() - 300;
+	var containerWidth = $(".container").width();
+	var coef;
+
+	//scroll functions
+	if (blocksHeight >= maxHeight) {
+		blocksHeight = maxHeight;
+	}
+	$(".details").height(blocksHeight);
+	$(".chat").height(blocksHeight);
+
+	//margin left function
+	var leftMargin = (windowWidth - containerWidth)/2 + windowClassWidth;
+	console.log("leftMargin: "+leftMargin);
+	var leftCenterMargin = (windowWidth - containerWidth)/2 + windowClassWidth + $(".details").width();
+	$(".details").css({"left": leftMargin});
+	$(".chat").css({"left": leftCenterMargin});
+
+	//adaptive block functions
+	if(windowWidth < 900) {
+		$(".details").hide();
+		$(".chat").hide();
+		$(".window").width(containerWidth);
+		console.log("minimum\n"+"windowWidth: "+windowWidth+"\ncontainerWidth: "+containerWidth);
+	} else if(windowWidth >= 900 && windowWidth < 1300) {
+		$(".details").show();
+		$(".chat").hide();
+
+		coef = containerWidth/2;
+		$(".window").width(coef*1.25);
+		$(".details").width(coef*0.75);
+		console.log("center\n"+"windowWidth: "+windowWidth+"\ncontainerWidth: "+containerWidth);
+	} else if (windowWidth >= 1300) {
+		$(".details").show();
+		$(".chat").show();
+		coef = containerWidth/4;
+		$(".window").width(coef*1.54);
+		$(".details").width(coef*1.23);
+		$(".chat").width(coef*1.23);
+		console.log("large\n"+"windowWidth: "+windowWidth+"\ncontainerWidth: "+containerWidth);
+	}
+}
+$(window).scroll(function() {
+	main();
+});
+
+$(window).resize(function() {
+	main();
+});
+
+$(document).ready(function() {
+	main();
+});
+
+
+
+
+
+
+
+$(".logo").click(function() {
+	main();
+});
